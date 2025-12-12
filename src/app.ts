ae6457f0cae6f9312232c3eee212e44db4b391d4
mayesha <soumyrhmn234@gmail.com>
2025-12-12 13:18:50 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ app.use("/api/v1", router)
 
 app.get("/", (req: Request, res: Response) => {
     res.status(200).json({
-        message: "Welcome to Tour Management System Backend"
+        message: "Welcome to Travel Buddy and Meetup System Backend"
     })
 })
 
